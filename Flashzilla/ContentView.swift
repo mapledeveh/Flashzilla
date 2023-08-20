3517fce202d07b5348cda157b8839b69ebb7f8c5
Alex Nguyen <alexviean@gmail.com>
2023-08-20 04:01:01 -0400

    Update Progress

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -7,120 +7,39 @@
 
 import SwiftUI
 
-
 struct ContentView: View {
-    @State private var offset = CGSize.zero
-    @State private var isDragging = false
-    
-    var body: some View {
-        let dragGesture = DragGesture()
-            .onChanged { value in
-                offset = value.translation
-            }
-            .onEnded { _ in
-                withAnimation {
-                    offset = .zero
-                    isDragging = false
-                }
-            }
-        
-        let pressGesture = LongPressGesture()
-            .onEnded { value in
-                withAnimation {
-                    isDragging = true
-                }
-            }
-        
-        let combined = pressGesture.sequenced(before: dragGesture)
-        
-        Circle()
-            .fill(.red)
-            .frame(width: 64, height: 64)
-            .scaleEffect(isDragging ? 1.5 : 1)
-            .offset(offset)
-            .gesture(combined)
-    }
-}
-
-struct MultipleGestureView: View {
     var body: some View {
         VStack {
-            Text("Hello, world!")
-                .onTapGesture {
-                    print("Text tapped")
-                }
+            Text("Hello")
+            Spacer().frame(height: 300)
+            Text("World")
         }
-//        .onTapGesture {
-//            print("VStack tapped")
-//        }
-//        .highPriorityGesture(
-        .simultaneousGesture(
-            TapGesture()
-                .onEnded {
-                    print("VStack tapped")
+        .contentShape(Rectangle())
+        .onTapGesture {
+            print("VStack tapped")
+        }
+    }
+}
+
+struct StackTap: View {
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 300, height: 300)
+                .onTapGesture {
+                    print("Rectangle tapped")
                 }
-        )
-    }
-}
-
-struct RotationEffectView: View {
-    @State private var currentAmount = Angle.zero
-    @State private var finalAmount = Angle.zero
-    
-    var body: some View {
-        TheButton()
-            .rotationEffect(currentAmount + finalAmount)
-            .gesture(
-                RotationGesture()
-                    .onChanged { angle in
-                        currentAmount = angle
-                    }
-                    .onEnded { angle in
-                        finalAmount += currentAmount
-                        currentAmount = .zero
-                    }
-            )
-    }
-}
-
-struct ScaleEffectView: View {
-    @State private var currentAmount = 0.0
-    @State private var finalAmount = 1.0
-    
-    var body: some View {
-        TheButton()
-            .scaleEffect(finalAmount + currentAmount)
-            .gesture(
-                MagnificationGesture()
-                    .onChanged { amount in
-                        currentAmount = amount - 1
-                    }
-                    .onEnded { amount in
-                        finalAmount += currentAmount
-                        currentAmount = 0
-                    }
-            )
-    }
-}
-
-struct PressGesture: View {
-    var body: some View {
-        TheButton()
-            .onLongPressGesture(minimumDuration: 2) {
-                print("Long pressed")
-            } onPressingChanged: { inProgress in
-                print("In progress: \(inProgress)")
-            }
-    }
-}
-
-struct TheButton: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-            .foregroundColor(.white)
-            .background(.blue)
-            .clipShape(RoundedRectangle(cornerRadius: 5))
+            
+            Circle()
+                .fill(.red)
+                .frame(width: 300, height: 300)
+                .onTapGesture {
+                    print("Circle tapped")
+                }
+                .contentShape(Rectangle())
+//                .allowsHitTesting(false)
+        }
     }
 }
 
